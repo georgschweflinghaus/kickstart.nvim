6b8e46cf932d3f7688b7991e201d9dc31fc2e02d
Georg Schweflinghaus <georgschwef>
2024-03-31 00:48:25 +0100

    Change the telescope key bindings to leader f...

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -25,9 +25,9 @@ local function telescope_live_grep_open_files()
   }
 end
 
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').find_files, { desc = '<space> Search Files' })
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader>b<space>', require('telescope.builtin').buffers, { desc = '[b][ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '<space> Search Files' })
+vim.keymap.set('n', '<leader>fo', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { desc = '[b][ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
