791fe149c553dfd1779fd005a055346f32fc6679
Georg Schweflinghaus <georgschwef>
2024-04-07 21:50:34 +0200

    feat: Moving lines in V mode added

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -56,4 +56,13 @@ vim.api.nvim_set_keymap('i', '<C-t>', '<Esc>:NERDTreeToggle<CR>', {noremap = tru
 -- Find the current buffer in NERDTree
 vim.api.nvim_set_keymap('n', '<leader>tf', ':NERDTreeFind<CR>', {noremap = true, silent = true})
 
-
+-- Move lines
+vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
+vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
+-- Move lines in visual mode with J and K
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
