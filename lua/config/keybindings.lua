151b53c3f3db3d0cfb3fb0251148ea5c083d916d
Georg Schweflinghaus <georgschwef>
2024-09-21 23:07:49 +0200

    add: new keybindings

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -3,10 +3,36 @@
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", {noremap=false})
+vim.api.nvim_set_keymap("n", "sr", ":%s/", { desc = 'Start search and replace sequence.'})
+
+-- Folding
+-- za zo zR zM
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- buffers
+vim.api.nvim_set_keymap("n", "tk", ":blast<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "tj", ":bfirst<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "th", ":bprev<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "tl", ":bnext<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "td", ":bdelete<enter>", {noremap=false})
+-- files
+vim.api.nvim_set_keymap("n", "QQ", ":q!<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "WW", ":w!<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "E", "$", {noremap=false})
+vim.api.nvim_set_keymap("n", "B", "^", {noremap=false})
+vim.api.nvim_set_keymap("n", "TT", ":TransparentToggle<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "ss", ":noh<CR>", {noremap=true})
+--
+-- splits
+--
+vim.api.nvim_set_keymap("n", "<C-W>,", ":vertical resize -10<CR>", {noremap=true})
+vim.api.nvim_set_keymap("n", "<C-W>.", ":vertical resize +10<CR>", {noremap=true})
+vim.keymap.set('n', '<space><space>', "<cmd>set nohlsearch<CR>")
+
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
